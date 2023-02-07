9ae03449cbe64331d3956da3a7005820160ba385
Vasu <vasucp1207@gmail.com>
2023-02-08 05:16:26 +0530

    add timedial

--- components.d.ts
+++ components.d.ts
@@ -9,6 +9,7 @@ declare module '@vue/runtime-core' {
     DarkSwitcher: typeof import('./src/components/DarkSwitcher.vue')['default']
     Navbar: typeof import('./src/components/Navbar.vue')['default']
     NavBar: typeof import('./src/components/NavBar.vue')['default']
+    TimeDial: typeof import('./src/components/TimeDial.vue')['default']
     TimeSchedular: typeof import('./src/components/TimeSchedular.vue')['default']
     TimeScheduler: typeof import('./src/components/TimeScheduler.vue')['default']
     Timezoneitem: typeof import('./src/components/Timezoneitem.vue')['default']
